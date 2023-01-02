86c2fff902046c88e3122491f0e1e7a7d20270fc
bruh <quangtrung02hn16@gmail.com>
2023-01-02 10:46:22 +0700

    Update dependencies. So much fun amirite.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@
 plugins {
     id("com.android.application").version("7.3.1").apply(false)
     id("com.android.library").version("7.3.1").apply(false)
-    kotlin("android").version("1.7.21").apply(false)
-    kotlin("multiplatform").version("1.7.21").apply(false)
+    kotlin("android").version("1.8.0").apply(false)
+    kotlin("multiplatform").version("1.8.0").apply(false)
 }
 
 tasks.register("clean") {
